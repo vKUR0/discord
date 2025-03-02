7509ab24be654381a5f9b304b4008588b464c21b
vKUR0 <cecille@et.esiea.fr>
2025-03-02 01:19:48 +0100

    DB end(1.32)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-
-export default function Home() {
+export default async function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="m-4">
+      <h1>Welcome to your new app!</h1>
     </div>
   );
 }
