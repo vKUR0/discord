7f834300d4c28b19772470b1c888f7c0738f08c6
vKUR0 <cecille@et.esiea.fr>
2025-03-03 02:10:16 +0100

    image

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "tailwindcss";
+import { withUt } from 'uploadthing/tw';
 
-const config: Config = {
+
+const config: Config =withUt({
     darkMode: ["class"],
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -59,5 +61,5 @@ const config: Config = {
   	}
   },
   plugins: [require("tailwindcss-animate")],
-};
+});
 export default config;
